08f083cba134ccf44cf70e02d5f70bb5f3c625e7
nasuda <nasuda908@gmail.com>
2024-01-02 15:17:58 +0900

    add public/hello/index.html

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
+app.use(express.static('public'))
+
+app.get('/express', (req, res) => {
   res.send('express')
 })
+
 
 
 app.listen(port, () => {
